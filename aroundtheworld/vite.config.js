e7ffb4ffc0f7ba33044eadfb03124819bf7d3100
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-10-11 23:12:06 +0200

    done

--- aroundtheworld/vite.config.js
+++ aroundtheworld/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    root: 'aroundtheworld', // Specifica la root corretta
     plugins: [react()],
     server: {
         port: 5173, // Imposta la porta qui se necessario
