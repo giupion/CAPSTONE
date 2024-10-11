e530ff8bb6a8cb1ce6602b210e03ab2b6d1c5618
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-10-11 23:01:14 +0200

    done

--- aroundtheworld/vite.config.js
+++ aroundtheworld/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 5173, // Imposta la porta qui se necessario
+    },
     resolve: {
         alias: {
             '@': '/resources/js',
         },
     },
-    plugins: [react()],
 });
